f196e8c734268e9150fe62231787ea46edc4de26
Nicolas Dunand <nico@kolya.ch>
2013-01-23 15:11:10 +0100

    Fixed version.php

--- version.php
+++ version.php
@@ -13,6 +13,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2011112601;       // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011120300;       // Requires this Moodle version
+$plugin->version   = 2013010100;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011120500;       // Requires this Moodle version
 $plugin->component = 'report_mergeusers'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '1.0';
+ 
