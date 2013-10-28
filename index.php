65c7f13d345f0c8e3067c5af65222b9cdd8458c2
Jordi Pujol Ahulló (32.1) <jordi.pujol@urv.cat>
2013-10-28 13:45:05 +0100

    moodle-mergeusers - adding control when to update records if we delete records for old user (cases grade_grades and groups_members).

--- index.php
+++ index.php
@@ -167,12 +167,25 @@ if ($data) {
             // Grades must be specially adjusted.
             /* pass $recordsToModify by reference so that the function can take care of some of our work for us */
             mergeGrades($newUser, $currentUser, $recordsToModify);
+            //ensure we have records to update
+            if (count($recordsToModify) == 0) {
+                //no records to update... go into the next table.
+                continue;
+            }
         }
         if($table_name == $CFG->prefix.'user_enrolments') {
             // User enrollments must be specially adjusted
             disableOldUserEnrollments($newUser, $currentUser);
             continue;
             // go onto next table
+        }
+        if($table_name == $CFG->prefix.'groups_members') {
+            mergeGroupMembers($newUser, $currentUser, $recordsToModify);
+            //ensure we have records to update
+            if (count($recordsToModify) == 0) {
+                //no records to update... go into the next table.
+                continue;
+            }
         }
 
         $idString = implode(', ', $recordsToModify);
