012561dfdd1ed01e521b3f403fc4fc40d33b95cd
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2022-12-14 20:05:29 +1100

    Update mod_assign phpunit base test

--- tests/assign_test.php
+++ tests/assign_test.php
@@ -23,6 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use mod_assign\base_test as mod_assign_base_testcase;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
