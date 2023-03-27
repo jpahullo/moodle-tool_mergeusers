78a3d03af433475addb2bcc604f81f699bc2fff2
nvallinoto <119407762+nvallinoto@users.noreply.github.com>
2023-03-27 18:58:05 +0200

    issue ndunand#218 - web services

--- db/upgrade.php
+++ db/upgrade.php
@@ -17,13 +17,14 @@
 /**
  * @package tool
  * @subpackage mergeusers
- * @author Jordi Pujol-Ahulló <jordi.pujol@urv.cat>
- * @copyright 2013 Servei de Recursos Educatius (http://www.sre.urv.cat)
+ * @author Jordi Pujol-Ahulló <jordi.pujol@urv.cat>, Nicola Vallinoto <n.vallinoto@liguriadigitale.it>
+ * @copyright 2013 Servei de Recursos Educatius (http://www.sre.urv.cat) - 2023 Liguria Digitale (https://www.liguriadigitale.it)
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /**
- * Take actions on upgrading mergeusers tool.
+ * Take actions on upgrading mergeusers tool. Any Moodle instance that is having this plugin already installed,
+ * must produce new table schema, and all existing data in old table (tool_mergeusers) should be migrated to the new table
  * @package tool_mergeusers
  * @global moodle_database $DB
  * @param int $oldversion old plugin version.
@@ -59,11 +60,49 @@ function xmldb_tool_mergeusers_upgrade ($oldversion) {
         // Conditionally launch create table for tool_mergeusersr
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
-        }
-
+        }        
+        
         // mergeusers savepoint reached
         upgrade_plugin_savepoint(true, 2013112912, 'tool', 'mergeusers');
     }
+
+    if ($oldversion > 2021072218) {
+      
+        // Define table tool_mergeusers_queue to be created.
+        $table = new xmldb_table('tool_mergeusers_queue');
+
+        // Adding fields to table tool_mergeusers_queue.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('removeuserfield', XMLDB_TYPE_CHAR, '50', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('removeuservalue', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('removeuserid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('keepuserfield', XMLDB_TYPE_CHAR, '50', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('keepuservalue', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('keepuserid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('timeadded', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('status', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('retries', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('log', XMLDB_TYPE_TEXT, null, null, null, null, null);
+
+        // Adding keys to table tool_mergeusers_queue.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        // Adding indexes to table tool_mergeusers_queue.
+        $table->add_index('mdl_toolmerg_ruv_ix', XMLDB_INDEX_NOTUNIQUE, ['removeuservalue']);
+        $table->add_index('mdl_toolmerg_kuv_ix', XMLDB_INDEX_NOTUNIQUE, ['keepuservalue']);
+        $table->add_index('mdl_toolmerg_tcs_ix', XMLDB_INDEX_NOTUNIQUE, ['timeadded', 'status']);
+        $table->add_index('mdl_toolmerg_tms_ix', XMLDB_INDEX_NOTUNIQUE, ['timemodified', 'status']);
+
+        // Conditionally launch create table for tool_mergeusers_queue.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // mergeusers savepoint reached
+        //upgrade_plugin_savepoint(true, 202107221, 'tool', 'mergeusers');
+    }
+
 
     return true;
 }
