79a9a15f36c2eff44df585f72ba9b06be8f26367
nvallinoto <n.vallinoto@liguriadigitale.it>
2023-06-09 11:41:34 +0200

    Merge user accounts - New web services #242

--- db/services.php
+++ db/services.php
@@ -14,27 +14,29 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /*
-* This file contains a merging request for a queue and. 
-* A function returning the list of the current queue of the merging requests.
+* This file contains a merge request for a queue and.
+* A function returning the list of the current queue of the merge requests.
 *
 * @package    merge_users
 * @copyright  2023 Liguria Digitale www.liguriadigitale.it
 * @author     Nicola Vallinoto <n.vallinoto@liguriadigitale.it>
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
+
 $functions = [
     // The name of your web service function, as discussed above.
-    'tool_mergeusers_enqueue_merging_request' => [
-        'classname'   => 'tool_mergeusers\external\enqueue_merging_request',
-        'methodname' => 'enqueue_merging_request',
-        'description' => 'Enqueue merging request',
+    'tool_mergeusers_enqueue_merge_request' => [
+        'classname'   => 'tool_mergeusers\external\enqueue_merge_request',
+        'methodname' => 'execute',
+        'description' => 'Enqueue merge request',
         'type'        => 'write',
         'ajax'        => true,
         ],
-    'tool_mergeusers_get_data_merging_requests' => [
-        'classname' => 'tool_mergeusers\external\get_data_merging_requests',
-        'methodname' => 'get_data_merging_requests',
-        'description' => 'Get data of merging requests.',
+    'tool_mergeusers_get_data_merge_requests' => [
+        'classname' => 'tool_mergeusers\external\get_data_merge_requests',
+        'methodname' => 'execute',
+        'description' => 'Get data of merge requests.',
         'type'        => 'read',
         'ajax'        => true,
     ]
