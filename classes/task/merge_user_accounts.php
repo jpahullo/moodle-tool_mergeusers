c09a47efc6197f0d11686ade74701db9908d26cb
nvallinoto <n.vallinoto@liguriadigitale.it>
2023-06-16 10:14:23 +0200

    Merge user accounts - New web services #242

--- classes/task/merge_user_accounts.php
+++ classes/task/merge_user_accounts.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 namespace tool_mergeusers\task;
+require_once(__DIR__ . '/../../lib/autoload.php');
 use MergeUserTool;
 use \tool_mergeusers\merge_request;
 /**
