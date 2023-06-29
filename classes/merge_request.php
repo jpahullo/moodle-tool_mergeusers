8e41d4c352078fc9ebc5ae3d5b5f3960577ae7e8
Nicola Vallinoto <n.vallinoto@liguriadigitale.it>
2023-06-29 16:48:53 +0200

    Web services working version

---
+++ classes/merge_request.php
@@ -0,0 +1,170 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information
+ *
+ * @package     tool
+ * @subpackage  mergeusers
+ * @author      Nicola Vallinoto, Liguria Digitale
+ * @author      Jordi Pujol-Ahulló, SREd, Universitat Rovira i Virgili
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace tool_mergeusers;
+class merge_request {
+    /**
+     * Missing merge request id. This id really does not exist.
+     * @var integer
+     */
+    const MISSING_MERGE_REQUEST_ID = 1;
+    /**
+     * Merge request queued but not yet processed.
+     * @var integer
+     */
+    const QUEUED_NOT_PROCESSED = 2;
+    /**
+     * Merge request queued, to be processed (the adhoc task is created for it).
+     * @var integer
+     */
+    const QUEUED_TO_BE_PROCESSED = 3;
+    /**
+     * Merge request is in progress but not concluded (its adhoc_task has started).
+     * @var integer
+     */
+    const INPROGRESS_NOT_CONCLUDED = 4;
+    /**
+     * Tried with error; pending of retrial (the merge process has found some error
+     * and the adhoc_task is queued to be retried in the future).
+     * @var integer
+     */
+    const TRIED_WITH_ERROR = 5;
+    /**
+     * Completed with success.
+     * @var integer
+     */
+    const COMPLETED_WITH_SUCCESS = 6;
+    /**
+     * Completed with errors (even with all the current retries the merge process
+     * could not end with success).
+     * @var integer
+     */
+    const COMPLETED_WITH_ERRORS = 7;
+    /**
+     * Reference table for merge requests.
+     * @var string
+     */
+    const TABLE_MERGE_REQUEST = 'tool_mergeusers_queue';
+     /**
+     * Reference table for merge requests. Before web service integration.
+     * @var string
+     */
+    const TABLE_MERGE_REQUEST_OLD = 'tool_mergeusers';
+    /**
+     * Reference table for users.
+     * @var string
+     */
+    const TABLE_USERS = 'user';
+    private $data;
+    private function __construct(stdClass $data) {
+        $this->data = $data;
+    }
+
+    public static function from(stdClass $data) {
+        return new self($data);
+    }
+
+    public function __get($name) {
+        if (isset($this->data->${name})) {
+            $value = $this->data->${name};
+            if ($name == 'log') {
+                $value = json_decode($value, true);
+            }
+            return $value;
+        }
+        return null;
+    }
+    public function __set($name, $value) {
+        if (isset($this->data->${name})) {
+            if ($name == 'log') {
+                $value = json_encode($value);
+            }
+            $this->data->${name} = $value;
+        }
+    }
+
+    public function convert_log_to_new_format() {
+        if (!isset($this->data->log) || empty($this->data->log)) {
+            return;
+        }
+        $logs = [];
+        $logs[1] = json_decode($this->data->log, true);
+        $this->data->log = json_encode($logs);
+    }
+
+    public function get_record(): \stdClass {
+        return $this->data;
+    }
+
+    public static function export_data_to_new_table(): void {
+        global $DB;
+        $sort = "id ASC";
+        $records = $DB->get_recordset(self::TABLE_MERGE_REQUEST_OLD,
+                                        null,
+                                        $sort);
+        if (!$records) {
+            // There is no need to migrate. That's all!
+            return;
+        }
+        $oldrequests = [];
+        $orderedoldrequests = [];
+        foreach ($records as $item) {
+            if (!isset($oldrequests[$item->fromuserid])) {
+                $oldrequests[$item->fromuserid] = [];
+            }
+            if (isset($oldrequests[$item->fromuserid][$item->touserid])) {
+                 $baseitem = $oldrequests[$item->fromuserid][$item->touserid];
+            } else {
+                $baseitem = new \stdClass();
+                $baseitem->removeuserfield = 'id';
+                $baseitem->removeuservalue = $item->fromuserid;
+                $baseitem->removeuserid = $item->fromuserid;
+                $baseitem->keepuserfield = 'id';
+                $baseitem->keepuservalue = $item->touserid;
+                $baseitem->keepuserid = $item->touserid;
+                if (isset( $item->mergedbyuserid)) {
+                    $baseitem->mergedbyuserid = $item->mergedbyuserid;
+                }
+                $baseitem->timeadded = $item->timemodified;
+                $baseitem->log = [];
+                $oldrequests[$item->fromuserid][$item->touserid] = $baseitem;
+                $orderedoldrequests[$baseitem->timeadded] = $baseitem;
+            }
+            // Old logs are obtained in ASC order, so we can safely update this values.
+            $baseitem->timemodified = $item->timemodified;
+            $baseitem->timecompleted = $item->timemodified;
+            // Append logs to the list.
+            $baseitem->log[$item->timemodified] = json_decode($item->log, false);
+            //$baseitem->log[0] = "Migrated from old record with id = ".$item->id;
+            $baseitem->status = ($item->status == 1) ? self::COMPLETED_WITH_SUCCESS : self::COMPLETED_WITH_ERRORS;
+            $baseitem->retries = 0;
+        }
+        // Insert ordered and simplified old records into new format.
+        foreach ($orderedoldrequests as $newrecord) {
+            $newrecord->log = json_encode($newrecord->log);
+            $DB->insert_record(merge_request::TABLE_MERGE_REQUEST, $newrecord);
+        }
+    }
+}
---
+++ classes/task/merge_user_accounts.php
@@ -0,0 +1,195 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+namespace tool_mergeusers\task;
+defined('MOODLE_INTERNAL') || die();
+require_once(__DIR__ . '/../../lib/autoload.php');
+use MergeUserTool;
+use \tool_mergeusers\merge_request;
+/**
+ * Adhoc task to merge user accounts.
+ *
+ * @package   tool_mergeusers
+ * @author    Jordi Pujol-Ahulló <jordi.pujol@urv.cat>
+ * @author    Nicola Vallinoto <n.vallinoto@liguriadigitale.it>
+ * @copyright 2013 Servei de Recursos Educatius (http://www.sre.urv.cat)
+ * @copyright 2023 Liguria Digitale (http://www.liguriadigitale.it)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * */
+class merge_user_accounts extends \core\task\adhoc_task {
+    /**
+     * Execute the task of merge users accounts.
+     */
+    public function execute() {
+        global $DB;
+        $data = $this->get_custom_data();
+        $maxattempts = get_config('tool_mergeusers', 'maxattempts');
+        $mergerequest = $DB->get_record(merge_request::TABLE_MERGE_REQUEST,
+                                            ['id' => $data->mergerequestid]);
+        if (!$mergerequest) {
+            // Merge request was removed before running this task.
+            return;
+        }
+        $lookatcriteriaforusers = $this->verify_users_to_keep_and_remove($mergerequest);
+        mtrace("User to keep: field = " .$record->keepuserfield. " and value = " .$record->keepuservalue. " ");
+        mtrace("User to remove: field = " .$record->removeuserfield. " and value = " .$record->removeuservalue. " ");
+        $mergerequest = $this->merge($record, $maxattempts, merge_request::TRIED_WITH_ERROR);
+        if ($mergerequestresult->status == merge_request::COMPLETED_WITH_SUCCESS) {
+            /* We run the merge request AGAIN because the user may be interacting with Moodle
+            * while merge request is being processed, so that NO ALL records are correctly migrated
+            * into the user to keep. It will ensure ALL records are correctly migrated into the user to keep. */
+            $mergerequestresult = $this->merge($mergerequestresult, $maxattempts, merge_request::COMPLETED_WITH_ERRORS);
+        }
+        if ($mergerequestresult->status != merge_request::COMPLETED_WITH_ERRORS &&
+            $mergerequestresult->status != merge_request::COMPLETED_WITH_SUCCESS) {
+            // Throwing exception will ensure this adhoc task is re-queued until $maxretries is reached.
+            throw new moodle_exception(get_string('failedmergerequest', 'tool_mergeusers'));
+        }
+    }
+    /**
+     * Function for merging two users.
+     */
+    private function merge(object $record, int $maxattempts, int $statusforerror) {
+        $retries = $record->retries + 1;
+        // Update retries.
+        $this->update_retries_in_table($record->id, $retries);
+        $logs = $record->log;
+        $mut = new MergeUserTool();
+        list($success, $log, $logid) = $mut->merge($record->keepuserid, $record->removeuserid);
+        if (!$success) {
+            if ($retries >= $maxattempts) {
+                $status = merge_request::COMPLETED_WITH_ERRORS;
+                // Send a notification to administrator ?
+            } else {
+                $status = $statusforerror;
+            }
+        }
+        $logs[$retries] = $log;
+        $this->update_status_and_log_in_table($record->id,
+                                            $status,
+                                            $logs);
+        $record->log = $logs;
+        $record->status = $status;
+        return $record;
+    }
+    /**
+     * Function for updating the status of the merging request.
+     */
+    private function update_status_and_log_in_table(int $idrecord,
+                                                    int $status,
+                                                    array $log): void {
+        global $DB;
+        if ($status = merge_request::COMPLETED_WITH_SUCCESS ||
+                    $status = merge_request::COMPLETED_WITH_ERRORS) {
+            $update = (object)[
+                'id' => $idrecord,
+                'status' => $status,
+                'timecompleted' => time(),
+                'log' => json_encode($log),
+            ];
+        } else {
+            $update = (object)[
+                'id' => $idrecord,
+                'status' => $status,
+                'log' => json_encode($log),
+            ];
+        };
+        $DB->update_record(
+            merge_request::TABLE_MERGE_REQUEST,
+            $update
+        );
+    }
+    /**
+     * Function for verifying the users to keep and remove.
+     */
+    private function verify_users_to_keep_and_remove(object $record): void {
+        global $DB;
+        $keepuserfield = $record->keepuserfield;
+        $keepuservalue = $record->keepuservalue;
+        $removeuserfield = $record->removeuserfield;
+        $removeuservalue = $record->removeuservalue;
+        // Verify user to remove.
+        $usertoremove = $DB->get_records(merge_request::TABLE_USERS, [$removeuserfield => $removeuservalue]);
+        if (is_null($record->removeuserid)) {
+            $this->removeuserid = $this->find_user_id_or_fail($record->removeuserfield, $record->removeuservalue);
+        }
+        // Verify user to keep.
+        $usertokeep = $DB->get_records(merge_request::TABLE_USERS, [$keepuserfield => $keepuservalue]);
+        if (is_null($record->keepuserid)) {
+            $this->keepuserid = $this->find_user_id_or_fail($record->keepuserfield, $record->keepuservalue);
+        }
+    }
+    /**
+     * Function for updating number of retries.
+     */
+    private function update_retries_in_table(int $idrecord,
+                                             int $retries): void {
+        global $DB;
+        $DB->update_record(
+            merge_request::TABLE_MERGE_REQUEST,
+            (object)[
+                'id' => $idrecord,
+                'retries' => $retries,
+                'timecompleted' >= time(),
+            ]
+        );
+    }
+    /**
+     * Function for updating removeuserid.
+     */
+    private function update_removeuserid_in_table(int $idrecord,
+                                                        int $removeuserid): void {
+        global $DB;
+        $DB->update_record(
+            merge_request::TABLE_MERGE_REQUEST,
+            (object)[
+                'id' => $idrecord,
+                'removeuserid' => $removeuserid
+            ]
+        );
+    }
+    /**
+     * Function for updating keepuserid.
+     */
+    private function update_keepuserid_in_table(int $idrecord,
+                                                int $keepuserid) {
+        global $DB;
+        $DB->update_record(
+            merge_request::TABLE_MERGE_REQUEST,
+            (object)[
+                'id' => $idrecord,
+                'keepuserid' => $keepuserid
+            ]
+        );
+    }
+    /**
+     * Function to find user id or fail.
+     */
+    protected function find_user_id_or_fail(int $mergerequestid, 
+                                            string $userfield, string $uservalue): int {
+        global $DB;
+        $users = $DB->get_records(merge_request::TABLE_USERS, [$userfield => $uservalue]);
+        if (count($users) == 0) {
+            throw new moodle_exception(get_string('cannotfinduser', 'tool_mergeusers',
+                                        (object)['userfield' => $userfield, 'uservalue' => $uservalue]));
+        }
+        if (count($users) > 1) {
+            throw new moodle_exception(get_string('toomanyusers', 'tool_mergeusers',
+                                        (object)['userfield' => $userfield, 'uservalue' => $uservalue]));
+        }
+        $user = reset($users);
+        return $user->id;
+    }
+}
