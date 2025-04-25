8631e54ae2de873b839c89448ddf87ccc0839031
Hoang, Minh Duc <minh-duc.hoang@fernuni-hagen.de>
2025-04-25 17:58:40 +0200

    #290 replace print_error by moodle_exception

--- lib/mergeusertool.php
+++ lib/mergeusertool.php
@@ -157,8 +157,11 @@ class MergeUserTool
                     cli_error('Error: ' . __METHOD__ . ':: ' . get_string('notablemergerclass', 'tool_mergeusers',
                                     $class));
                 } else {
-                    print_error('notablemergerclass', 'tool_mergeusers',
-                            new moodle_url('/admin/tool/mergeusers/index.php'), $class);
+                    throw new moodle_exception(
+                        'notablemergerclass',
+                        'tool_mergeusers',
+                        (new moodle_url('/admin/tool/mergeusers/index.php'))->out(),
+                        $class);
                 }
             }
             // Append any additional table to skip.
@@ -415,8 +418,11 @@ class MergeUserTool
                 cli_error('Error: ' . __METHOD__ . ':: ' . get_string('errortransactionsonly', 'tool_mergeusers',
                                 $CFG->dbtype));
             } else {
-                print_error('errortransactionsonly', 'tool_mergeusers',
-                        new moodle_url('/admin/tool/mergeusers/index.php'), $CFG->dbtype);
+                throw new moodle_exception(
+                    'errortransactionsonly',
+                    'tool_mergeusers',
+                    (new moodle_url('/admin/tool/mergeusers/index.php'))->out(),
+                    $CFG->dbtype);
             }
         }
 
