8699194c1d9191d418d69f8d5e0be7d92b012454
Nihaal Shaikh <nihaal.shaikh@ldesignmedia.nl>
2025-11-04 13:32:08 +0100

    Update #86c69jmtu #BSLA-514 - Version bump

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026052709;
-$plugin->release = '(2026052709: Focus on stability and extensibility)';
+$plugin->version = 2026052710;
+$plugin->release = '(2026052710: Focus on stability and extensibility)';
 $plugin->requires = 2024100700; // Moodle 4.5+, https://moodledev.io/general/releases#moodle-45-lts.
 $plugin->component = 'tool_mergeusers';
 $plugin->maturity = MATURITY_STABLE;
